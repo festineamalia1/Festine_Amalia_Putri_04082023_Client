8a94cf274eb8a6ad314755d86b3e2cfe73cdff43
Festine-Capio <festine@capioteknologi.co.id>
2023-08-05 14:07:03 +0700

    Updates

---
+++ precache-manifest.a46295d151b1fa55921f5e3d8dbe37cd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcda15ecfa4e5607a22d4ab5276919ac",
+    "url": "/Festine_Amalia_Putri_04082023_Client/index.html"
+  },
+  {
+    "revision": "83ef2652f50175911abd",
+    "url": "/Festine_Amalia_Putri_04082023_Client/static/css/main.13affa68.chunk.css"
+  },
+  {
+    "revision": "631c70a2a37d0cb633e0",
+    "url": "/Festine_Amalia_Putri_04082023_Client/static/js/2.ae4aeb56.chunk.js"
+  },
+  {
+    "revision": "d7d5d466de08c7c189c349ac9206fbcf",
+    "url": "/Festine_Amalia_Putri_04082023_Client/static/js/2.ae4aeb56.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83ef2652f50175911abd",
+    "url": "/Festine_Amalia_Putri_04082023_Client/static/js/main.6eda9053.chunk.js"
+  },
+  {
+    "revision": "5dd8975803f86b7efcd5",
+    "url": "/Festine_Amalia_Putri_04082023_Client/static/js/runtime-main.7a2c0f71.js"
+  },
+  {
+    "revision": "d84593e91b5a4ff4a1c19902c662122b",
+    "url": "/Festine_Amalia_Putri_04082023_Client/static/media/logout.d84593e9.svg"
+  }
+]);
